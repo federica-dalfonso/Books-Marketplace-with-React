7960930220426fa46b550798107c6ac198eb5fd5
Federica D'Alfonso <federica.dalfonso@gmail.com>
2024-03-08 20:09:13 +0100

    aggiunto shadow all'Alert

--- epibooks/src/components/WelcomeAlert/WelcomeAlert.jsx
+++ epibooks/src/components/WelcomeAlert/WelcomeAlert.jsx
@@ -5,8 +5,8 @@ export default function WelcomeAlert () {
     return (
         <div className='d-flex justify-content-center mt-4'>
             <Alert className='welcome-message d-flex flex-column align-items-center'>
-                <span className='fs-5 mb-4'>Welcome to my first React App!</span>
-                <span className='fs-5 text-uppercase'>epibooks</span>
+                <span className='fs-6 mb-4'>Welcome to my first React App!</span>
+                <h1 className='text-uppercase'>epibooks</h1>
             </Alert>
         </div>
     )
