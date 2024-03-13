a4b6b495e2d3d600285b5236f2dde1a7f1e47bd5
Federica D'Alfonso <federica.dalfonso@gmail.com>
2024-03-13 13:07:22 +0100

    week2D1 - inputSearch & stato  + EXTRA

--- epibooks/src/components/Main/AllBooks.jsx
+++ epibooks/src/components/Main/AllBooks.jsx
@@ -1,19 +1,35 @@
 import './AllBooks.css'
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Form } from 'react-bootstrap';
 import jsonData from '../../data/fantasy.json';
 import SingleBook from './SingleBook';
+import { useState } from 'react';
 
 
-export default function AllBooks ({ img, title, price }) {       
+export default function AllBooks ({ img, title, price }) {    
+    
+    const [inputName, setInputName] = useState("");
+
+    {/*filtro ricerca */}
+    const searchResult = jsonData.filter((element) => {
+        return element.title.toLowerCase().includes(inputName.toLowerCase())
+    });
+
     return (
         <>
-            <Container className='my-4'>                        
+            <Container className='my-4'> 
+
+                {/* componente input text */}
+                <Form.Control className= "input-field my-4"
+                type="text" value={inputName} id="inputSearch"
+                placeholder="Cerca un titolo..."
+                onChange={(e) => setInputName(e.target.value)}/>
+
                 <Row>
-                    {jsonData.map((book) => {
-                       return (
-                       <SingleBook key= {book.asin} image= {book.img} title= {book.title} price= {`${book.price} euro`}/>
-                       )})}
+                    {searchResult.map((book) => (
+                        <SingleBook key={book.asin} image={book.img} title={book.title} price={`${book.price} euro`}/>
+                    ))}
                 </Row>
+
             </Container>  
         </>
     )
--- epibooks/src/components/Main/SingleBook.jsx
+++ epibooks/src/components/Main/SingleBook.jsx
@@ -1,13 +1,19 @@
 import "./SingleBook.css"
 import { Col, Card } from "react-bootstrap"
-
+import { useState } from 'react'
 
 export default function SingleBook ( {image, title, price }) {
+
+    const [selected, setSelected] = useState(false);
+    const coverClick = () => {
+        setSelected(true);
+    }
+
     return (
         <>
         <Col sm={6} md={3} lg={3} className='g-4'>
-            <Card className='h-100 cursor-hover border-color'>
-                <Card.Img src={image} className='card-image'/>
+            <Card className={`h-100 cursor-hover border-color`}>
+                <Card.Img src={image} className={`card-image ${selected ? 'selected-red' : ''}`} onClick={coverClick}/>
                 <Card.Body className="p-2 d-flex flex-column align-items-center">
                     <Card.Title className='fs-6 text-center ellipsis'>{title}</Card.Title>
                     <Card.Text className="fw-lighter py-1 px-2">{price}</Card.Text>
