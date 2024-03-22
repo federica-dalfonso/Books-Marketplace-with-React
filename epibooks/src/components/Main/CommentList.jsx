821cf28b887f206262a34e0b19b75249c872df5a
Federica D'Alfonso <federica.dalfonso@gmail.com>
2024-03-22 18:30:28 +0100

    aggiunto ThemeContext ad Area Commenti

--- epibooks/src/components/Main/CommentList.jsx
+++ epibooks/src/components/Main/CommentList.jsx
@@ -1,6 +1,9 @@
 import './CommentList.css'
+import React, { useContext } from 'react';
+import { ThemeContext } from "../../context/ThemeContextProvider";
 
 export default function CommentList () {
+    const { theme } = useContext(ThemeContext);
 
-    return <h1>Comment</h1>
+    return <h1 className={theme === "dark" ? "text-light" : "text-dark"}>Comment</h1>
 }
